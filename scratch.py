3cb56d6a0db83136d3d1739e36a9d1c11ecaa447
blacksheepaway <50200471+blacksheepaway@users.noreply.github.com>
2019-05-04 07:07:57 -0300

    Calculadora de IMC simples

---
+++ scratch.py
@@ -0,0 +1,28 @@
+# Calculadora de IMC
+
+print("CALCULADORA DE IMC \n \n")
+nome = input(("Qual seu nome? "))
+print("Bem vindo(a), {0}!".format(nome))
+altura = float(input("Digite sua altura em metros: "))
+peso = float(input("Digite seu peso em Kg: "))
+
+imc = peso / altura**2
+
+print("Seu IMC é: %.2f" % imc)
+
+if imc < 16:
+	print("Magreza grave")
+elif imc < 17:
+	print("Magreza moderada")
+elif imc < 18.5:
+	print("Magreza leve")
+elif imc < 25:
+	print("Saudável")
+elif imc < 30:
+	print("Sobrepeso")
+elif imc < 35:
+	print("Obesidade Grau I")
+elif imc < 40:
+	print("Obesidade Grau II (severa)")
+else:
+    print("Obesidade Grau III (mórbida)")
